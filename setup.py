424c89701b0bfeb2e9365006b7441f3a16f009eb
Daniel Alkemic Czuba <alkemic7@gmail.com>
2015-12-06 08:36:51 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='Scrapper',
+    version='0.0.1',
+    url='https://github.com/Alkemic/scrapper',
+    license='MIT',
+    author='Daniel Alkemic Czuba',
+    author_email='alkemic7@gmail.com',
+    description='Scrapper is small, Python web scrapping library',
+    py_modules = ['scrapper'],
+    keywords='scrapper,webscrapping',
+    install_requires=[
+        'beautifulsoup4==4.3.2',
+        'requests == 2.5.1',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
