eb297eaff6177e21b0f7ac7032f307ae3ab146c5
Daniel Alkemic Czuba <alkemic7@gmail.com>
2015-12-07 16:57:24 +0100

    Tweaking item set class

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import re
 import copy
 from datetime import datetime
 from time import sleep
@@ -16,6 +17,17 @@ HEADERS = {
 }
 
 FETCH_DATA_DELAY = 0.1
+
+REGEXP_LINK = re.compile(
+    r'^(?:http|ftp)s?://'  # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
+    r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+    r'localhost|'  # localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+    r'(?::\d+)?'  # optional port
+    r'(?:/?|[/?]\S+)$',
+    re.IGNORECASE,
+)
 
 
 class ScrapperException(Exception):
@@ -177,53 +189,71 @@ class CrawlerItemSet(object):
     item_class = None
 
     def __init__(self):
+        if not self.next_selector and not self.links_selector:
+            raise ScrapperException(
+                'You should define either`links_selector` or `next_selector`'
+            )
+
+        if self.links_selector is not None and \
+                not isinstance(self.links_selector, (tuple, list)):
+            self.links_selector = self.links_selector,
+
+        if self.next_selector is not None and \
+                not isinstance(self.next_selector, (tuple, list)):
+            self.next_selector = self.next_selector,
+
         if not self.item_class:
             raise ScrapperException('You need to setup `item_class`')
 
         if not issubclass(self.item_class, (CrawlerItem, CrawlerMultiItem)):
-            raise ScrapperException('`item_class` need to be instance of '
-                                    '`CrawlerItem` or `CrawlerMultiItem`')
+            raise ScrapperException(
+                '`item_class` need to be instance of `CrawlerItem` or '
+                '`CrawlerMultiItem`'
+            )
 
         if not self.next_selector and not self.links_selector:
-            raise ScrapperException('You need to setup `next_selector` '
-                                    'or `self.links_selector`')
+            raise ScrapperException(
+                'You need to setup `next_selector` or `self.links_selector`'
+            )
 
         self.response = fetch_data(self.url)
         self.content = self.response.content
 
     def __iter__(self):
         soup = BeautifulSoup(self.content)
-
         if self.next_selector:
+            yield self.item_class(self.url, self, self.content)
             while True:
-                if isinstance(self.next_selector, tuple):
-                    selected_next = soup.find_all(*self.next_selector)
-                else:
-                    selected_next = soup.find_all(self.next_selector)
-
+                selected_next = soup.find_all(*self.next_selector)
                 if len(selected_next) == 0:
                     raise ScrapperCantFindNext(
                         'Couldn\'t find element by selector "{}"'.format(
-                            self.next_selector
+                            self.next_selector,
                         )
                     )
 
                 next_link = selected_next[0]
-
-                if self.base_url in next_link.attrs['href']:
-                    next_url = next_link.attrs['href']
-                else:
-                    next_url = self.base_url + next_link.attrs['href']
-
-                yield self.item_class(next_url, self, self.content)
+                next_url = next_link.attrs['href']
+                if not REGEXP_LINK.match(next_url):
+                    next_url = self.base_url + next_url
 
                 soup = BeautifulSoup(self.content)
                 self.content = fetch_data(next_url).content
 
-        for link in soup.select(self.links_selector):
-            if self.base_url in link.attrs['href']:
-                link_url = link.attrs['href']
-            else:
-                link_url = self.base_url + link.attrs['href']
+                yield self.item_class(next_url, self)
 
-            yield self.item_class(link_url, self)
+        if self.links_selector:
+            selected_links = soup.find_all(*self.links_selector)
+            if len(selected_links) == 0:
+                raise ScrapperCantFindNext(
+                    'Couldn\'t find element by selector "{}"'.format(
+                        self.next_selector
+                    )
+                )
+            for link in selected_links:
+                if self.base_url in link.attrs['href']:
+                    link_url = link.attrs['href']
+                else:
+                    link_url = self.base_url + link.attrs['href']
+
+                yield self.item_class(link_url, self)
